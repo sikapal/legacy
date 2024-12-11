d684f7fa084c0c06f51357063b26b77d775a9b35
sikapa <sikapalucien.lucien@gmail.com>
2024-12-11 23:43:52 +0100

    mounir offcial pic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import banner1 from "../assets/team-01.jpeg"
+import banner1 from "../assets/team-01.jpg"
 
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
                 <div className='w-full mx-2 py-4 duration-8000'>
                     <div className="my-28 md:my-8 md:mx-2 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
                         <div className=' '>
-                            <img className='mt-6 sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px]  rounded-full lg:w-[450px] lg:h-[500px] pr-4 ' src={banner1} alt="img" />
+                            <img className='mt-6 sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px]  rounded-full lg:w-[450px] lg:h-[600px] pr-4 ' src={banner1} alt="img" />
                         </div>
                         {/*hero text here*/}
 
@@ -18,7 +18,7 @@ const Home = () => {
                             <h1 className=' lg:text-8xl font-semibold sm:text-xl text-white md:w-3/4 -mt-2 leading-snug'>AHMED MOUNIR LINJOUOM
 
                             </h1>
-                            <p className='text-white text-base  mt-1 italic  justify-center'>Avec SUP'PTIC LEGACY, Réécrivons l'histoire et créons un héritage </p>
+                            <p className='text-white text-base hidden md:block mt-1 italic  justify-center'>Avec SUP'PTIC LEGACY, Réécrivons l'histoire et créons un héritage </p>
 
                             <div className='flex flex-row  mt-0 sm:mt-8'>
 
